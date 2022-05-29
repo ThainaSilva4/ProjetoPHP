0c1c25e04ff189fecd2e90e1782e92fc7171333c
Thainá Ferreira <thaina.silva254@etec.sp.gov.br>
2022-05-29 17:21:03 -0300

    Projeto PHP - Loops e troca de página

---
+++ tela_principal.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+       <title>PÁGINA PRINCIPAL</title>
+    </head>
+  <body>
+  <h1>MENU DE OPÇÕES</h1><BR>
+  <a href = "opcao.php?tela=01">TELA 01</a>&nbsp;
+  <a href = "opcao.php?tela=02">TELA 02</a>&nbsp;
+  <a href = "opcao.php?tela=03">TELA 03</a><br><br>
+
+  <h3>PÁGINA PRINCIPAL</h3>
+  </body>
+
+</html>
